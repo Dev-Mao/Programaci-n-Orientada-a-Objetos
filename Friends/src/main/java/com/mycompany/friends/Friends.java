13a91e6e44bdd0a9c2858f4eb64a21c4490358d7
Mariana Agudelo <agudelomariana2503@gmail.com>
2024-09-19 11:58:23 -0500

    Add files via upload

---
+++ Friends/src/main/java/com/mycompany/friends/Friends.java
@@ -0,0 +1,9 @@
+package com.mycompany.friends;
+
+public class Friends {
+
+    public static void main(String[] args) {
+        MainForm frm = new MainForm();
+        frm.setVisible(true);
+    }
+}
